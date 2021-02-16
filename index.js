65df24c2c56b44f63ae0a2c0861f45e804f90dbd
PJEder12 <jeder@uninorte.edu.co>
2021-02-16 13:09:18 -0500

    Agregando Middleware al Servidor

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ const app = express();
 const { config } = require('./config/index');
 const { database } = require('./config/dbConfig');
 
+//Middlewares:
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', function(req, res) {
     res.send('Hello Wolrd');
 });
 
-database.query('SELECT * FROM InventoryManagement.Usuario', async(err,result) => {
+database.query('SELECT * FROM InventoryManagement.Usuarios', async(err,result) => {
     if (err) throw err;
     user=result[0];
     console.log(user);
